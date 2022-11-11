0043c291e535d432c0ede3887c4c75c6146ed866
kaumudi <kaumudi766@gmail.com>
2022-11-11 17:12:29 +0530

    this is commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # terraform_statefile
+# How to manage Terraform State ?
+This article would help us to create a S3 bucket and DynamoDB table using terraform in a single file.We shall Know about following topic:-
+1.Terraform State.
+2.Shared storage for state files.
+3.Limitations with terraform Backends.
+I got information from this [article](https://blog.gruntwork.io/how-to-manage-terraform-state-28f5697e68fa#.r6xdvtxqe) and the code is explained in this 
+[video](https://www.youtube.com/watch?v=Cn6xYf0QJME)
+>Prerequirement:-
+
+- install terraform [video](https://shorthillstech-my.sharepoint.com/personal/kapil_jain_shorthillstech_com/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2Fkapil%5Fjain%5Fshorthillstech%5Fcom%2FDocuments%2FTraining%2FDevOps%2F2022%2F45%2Fterraform%20state%5Fkaumudi%2Emp4&parent=%2Fpersonal%2Fkapil%5Fjain%5Fshorthillstech%5Fcom%2FDocuments%2FTraining%2FDevOps%2F2022%2F45)
--- main.tf
+++ main.tf
@@ -5,3 +5,55 @@ resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
 }
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "kaumudi-tf-test-bucketstate-1"
+ 
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "DynamoDB-Terraform"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "kaumudi-tf-test-bucketstate-1"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "DynamoDB-Terraform"
+    encrypt        = true
+  }
+}
